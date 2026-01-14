3e723c8ada93bb71cf69c05bead43fa664d05b62
Joreynoso <joreynoso.developer@gmail.com>
2026-01-14 19:55:24 -0300

    fix: fix skeleton componenet to match with task component page.

--- app/projects/[id]/loading.tsx
+++ app/projects/[id]/loading.tsx
@@ -1,23 +1,27 @@
 import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Loading() {
-  return (
-    <div className='w-full min-h-[calc(100vh-4rem)] mx-auto flex flex-col py-5 pb-10 max-w-7xl'>
-       {/* Replicar el Header/Breadcrumb con skeletons */}
-       <div className="mb-5 flex flex-col gap-y-2">
-         <Skeleton className="h-5 w-32 bg-secondary" /> {/* Breadcrumb fake */}
-           </div>
+   return (
+      <div className='w-full min-h-[calc(100vh-4rem)] mx-auto flex flex-col py-5 pb-10 max-w-7xl'>
+         {/* Replicar el Header/Breadcrumb con skeletons */}
+         <div className="mb-5 flex flex-col gap-y-2">
+            <Skeleton className="h-5 w-32 bg-secondary" /> {/* Breadcrumb fake */}
+         </div>
 
-       <div className="flex justify-between items-center mb-6">
-          <Skeleton className="h-10 w-40 bg-secondary" /> {/* Botón agregar fake */}
-       </div>
+         <div className='w-full flex flex-col sm:flex-row gap-4 mb-4'>
+            <Skeleton className='w-full h-[125px] bg-secondary rounded-xl'>
+            </Skeleton>
 
-       {/* Grid de skeletons simulando tarjetas */}
-       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {[1, 2, 3].map((i) => (
-             <Skeleton key={i} className="h-[700px] w-full rounded-xl bg-secondary" />
-          ))}
-       </div>
-    </div>
-  )
+            <Skeleton className='w-full h-[125px] bg-secondary rounded-xl'>
+            </Skeleton>
+         </div>
+
+         {/* Grid de skeletons simulando tarjetas */}
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {[1, 2, 3].map((i) => (
+               <Skeleton key={i} className="h-[700px] w-full rounded-xl bg-secondary" />
+            ))}
+         </div>
+      </div>
+   )
 }
