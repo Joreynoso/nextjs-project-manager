bd887e658d3b0de8856ca715553c977c77b035c2
Joreynoso <joreynoso.developer@gmail.com>
2026-01-14 21:48:27 -0300

    feat: Add client-side authentication utility and update gitignore to exclude `.env.local`.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,9 @@
 import { createAuthClient } from "better-auth/react"
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/auth',
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL 
+      ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`
+      : 'http://localhost:3000/api/auth',
 })
 
 export const { signIn, signOut, signUp, useSession, getAccessToken } = authClient
