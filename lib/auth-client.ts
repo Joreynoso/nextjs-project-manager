bbee784e4127852389dcc076fc9d50db2ef03d9f
Joreynoso <joreynoso.developer@gmail.com>
2026-01-14 21:48:51 -0300

    feat: Add `better-auth` client configuration for authentication.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 
+// cambiara local si quiero usar la api local
 export const authClient = createAuthClient({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL 
       ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`
